d7143c90becb86e65b9c31de1b4be92e83db4f2b
Samedi <alexclxud@gmail.com>
2025-10-07 00:15:38 +0300

    Added login page class, locators, changed base page and main paige classes and test_main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,17 +1,20 @@
-import time
-import math
-import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 class BasePage():
-   def __init__(self, browser, url):
+   def __init__(self, browser : webdriver.Chrome, url, timeout = 10):
       self.browser = browser
       self.url = url
+      self.browser.implicitly_wait(timeout)
+
+   def is_element_present(self, how, what):
+    try:
+        self.browser.find_element(how, what)
+    except (NoSuchElementException):
+        return False
+    return True
     
    def open(self):
       self.browser.get(self.url)                                 
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,15 +1,11 @@
-import time
-import math
-import pytest
-
 from .base_page import BasePage
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage): 
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
