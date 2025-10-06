99bc0df891245fd883fae200aae69ffe42a13891
Samedi <alexclxud@gmail.com>
2025-10-06 21:15:30 +0300

    changed test_main paige

--- test_main_page.py
+++ test_main_page.py
@@ -6,10 +6,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_items(browser):
-    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
-    browser.get(link)
-    browser.implicitly_wait(10)
-    time.sleep(5)
-    elems = browser.find_elements(By.CSS_SELECTOR, '.btn-add-to-basket')
-    assert len(elems) == 1, 'Элемент не найден' if len(elems) == 0 else 'Найдено несколько элементов'
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
+    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
+    login_link.click()
+
+def test_guest_can_go_to_login_page(browser): 
+   browser.get(link) 
+   go_to_login_page(browser)                               
+ 
